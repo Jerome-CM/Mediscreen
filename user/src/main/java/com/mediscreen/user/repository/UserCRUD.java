0a091e05c06627624ea9c1ab8cbc914e81ee4519
Bouteveille jerome <formation@community-technologie.fr>
2023-04-20 15:33:28 +0200

    Add findUserByLogin

--- user/src/main/java/com/mediscreen/user/repository/UserCRUD.java
+++ user/src/main/java/com/mediscreen/user/repository/UserCRUD.java
@@ -1,2 +1,11 @@
-package com.mediscreen.user.repository;public interface userCRUD {
+package com.mediscreen.user.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserCRUD extends MongoRepository<Object, Long> {
+
+    public Optional<Object> findUserByLogin(String login);
+
 }
