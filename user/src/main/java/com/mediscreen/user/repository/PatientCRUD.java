143c3d475a4153b286773372214972fe72e2ce9f
Bouteveille jerome <formation@community-technologie.fr>
2023-04-25 08:06:12 +0200

    Add findPatientByFirstnameAndLastnameAndBirthdate

--- user/src/main/java/com/mediscreen/user/repository/PatientCRUD.java
+++ user/src/main/java/com/mediscreen/user/repository/PatientCRUD.java
@@ -4,6 +4,11 @@ import com.mediscreen.user.entity.Patient;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PatientCRUD extends MongoRepository<Patient, Long> {
+
+     public Optional<Patient> findPatientByFirstnameAndLastnameAndBirthdate(String firstname, String lastname, String birthdate);
+
 }
