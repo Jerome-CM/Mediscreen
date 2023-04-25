ee7872f8d7f579dac60ba3eb0584c80d485be70d
Bouteveille jerome <formation@community-technologie.fr>
2023-04-25 07:50:09 +0200

    Add @EnableFeignClients

--- front/src/main/java/com/mediscreen/front/FrontApplication.java
+++ front/src/main/java/com/mediscreen/front/FrontApplication.java
@@ -2,6 +2,7 @@ package com.mediscreen.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.mediscreen.front")
