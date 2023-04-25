3bacd839cba480fc55be7471433c8d95ff943379
Bouteveille jerome <formation@community-technologie.fr>
2023-04-25 07:50:47 +0200

    Add save and update mapping

--- front/src/main/java/controller/AuthController.java
+++ front/src/main/java/controller/AuthController.java
@@ -1,2 +1,52 @@
-package controller;public class AuthController {
+package controller;
+
+import beans.ConnexionBean;
+import beans.RegisterBean;
+import com.mediscreen.user.dto.Response;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import proxies.AuthProxy;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class AuthController {
+
+    private final AuthProxy authProxy;
+
+    public AuthController(AuthProxy authProxy) {
+        this.authProxy = authProxy;
+    }
+
+    @GetMapping(value="/")
+    public String home(){
+        return "login";
+    }
+
+    @GetMapping(value="register")
+    public String getRegister(Model map){
+        map.addAttribute("ConnexionDTO", new ConnexionBean());
+        map.addAttribute("RegisterDTO", new RegisterBean());
+        return "login";
+    }
+
+    @GetMapping(value="login")
+    public String getlogin(Model map){
+        map.addAttribute("ConnexionDTO", new ConnexionBean());
+        map.addAttribute("RegisterDTO", new RegisterBean());
+        return "login";
+    }
+
+    @PostMapping(value="login")
+    public Response login(HttpServletRequest request){
+        return authProxy.login(request);
+    }
+
+    @PostMapping(value="register")
+    public Response register(HttpServletRequest request){
+        return authProxy.register(request);
+    }
 }
--- front/src/main/java/controller/PatientController.java
+++ front/src/main/java/controller/PatientController.java
@@ -1,12 +1,19 @@
 package controller;
 
 import beans.PatientBean;
+import com.mediscreen.user.dto.Response;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import proxies.UserProxy;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -14,25 +21,70 @@ public class PatientController {
 
     private final UserProxy userProxy;
 
-    public PatientController(UserProxy userProxy) {
+    private final ModelMapper modelMapper;
+
+    public PatientController(UserProxy userProxy, ModelMapper modelMapper) {
         this.userProxy = userProxy;
+        this.modelMapper = modelMapper;
     }
-    @GetMapping(value="/patientList")
-    public String getPatientsList(Model map){
-        List<PatientBean> patients = userProxy.getPatientsList();
-        map.addAttribute("patients", patients);
-        return "patientList";
+
+    @GetMapping(value="/addPatient")
+    public String getAddPatient(Model map){
+        map.addAttribute("patient", new PatientBean());
+        return "addPatient";
+    }
+
+    @PostMapping(value="/addPatient")
+    public void addPatient(HttpServletRequest request){
+        userProxy.addPatient(request);
+    }
+
+    @GetMapping(value="/updatePatient/{firstname}{lastname}{birthdate}")
+    public String getUpdatePatient(Model map,
+                                   @RequestParam("firstname") String firstname,
+                                   @RequestParam("lastname") String lastname,
+                                   @RequestParam("birthdate") String birthdate){
+        Response response = userProxy.getPatient(firstname, lastname, birthdate);
+        if(response.getStatus().equals("Ok")) {
+            PatientBean patientBean = modelMapper.map(response.getContent(), PatientBean.class);
+            map.addAttribute("patient", patientBean);
+        } else {
+            map.addAttribute("response", response);
+        }
+        return "updatePatient";
+    }
+
+    @PostMapping(value="/updatePatient")
+    public void updatePatient(HttpServletRequest request){
+        userProxy.updatePatient(request);
     }
 
     @GetMapping(value="/patient")
-    public String getPatients(Model map, long id){
-        PatientBean patient = userProxy.getPatient(id);
-        map.addAttribute("patient", patient);
+    public String getPatientDetails(Model map,
+                                    @RequestParam("firstname") String firstname,
+                                    @RequestParam("lastname") String lastname,
+                                    @RequestParam("birthdate") String birthdate){
+        Response response = userProxy.getPatient(firstname, lastname, birthdate);
+        if(response.getStatus().equals("Ok")) {
+            PatientBean patientBean = modelMapper.map(response.getContent(), PatientBean.class);
+            map.addAttribute("patient", patientBean);
+        } else {
+            map.addAttribute("response", response);
+        }
         return "patient";
     }
 
-    @PostMapping(value="addPatient")
-    public void addANewPatient(PatientBean patient){
-        // TODO Methode save ou methode de User qui appelle save ?
+    @GetMapping(value="/patientList")
+    public String getPatientsList(Model map){
+        Response response = userProxy.getPatientsList();
+        if(response.getStatus().equals("Ok")){
+            List<PatientBean> patientBeanList = (List<PatientBean>) response.getContent();
+            map.addAttribute("patients", patientBeanList);
+        } else {
+            map.addAttribute("response", response);
+        }
+        return "patientList";
     }
+
+
 }
