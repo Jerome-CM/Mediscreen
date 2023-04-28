022c914498da39e4b7238a1874d1865296cfbff6
Bouteveille jerome <formation@community-technologie.fr>
2023-04-28 13:37:41 +0200

    Add interface

--- user/src/main/java/com/mediscreen/user/service/DoctorService.java
+++ user/src/main/java/com/mediscreen/user/service/DoctorService.java
@@ -1,2 +1,20 @@
-package com.mediscreen.user.service;public interface DoctorService {
+package com.mediscreen.user.service;
+
+import com.mediscreen.user.dto.DoctorDTO;
+import com.mediscreen.user.dto.Response;
+import com.mediscreen.user.entity.Doctor;
+
+public interface DoctorService {
+
+    Response findDoctorByLogin(String login);
+
+    Response saveDoctor(DoctorDTO doctorDTO);
+
+    Doctor getDoctorById(Long id);
+
+    Response auth(DoctorDTO doctorDTO);
+
+
+
+
 }
--- user/src/main/java/com/mediscreen/user/service/PatientService.java
+++ user/src/main/java/com/mediscreen/user/service/PatientService.java
@@ -1,2 +1,19 @@
-package com.mediscreen.user.service;public interface PatientService {
+package com.mediscreen.user.service;
+
+import com.mediscreen.user.dto.PatientDTO;
+import com.mediscreen.user.dto.Response;
+
+import java.math.BigInteger;
+
+public interface PatientService {
+
+    Response savePatient(PatientDTO patientDTO);
+
+    Response updatePatient(PatientDTO patientDTO);
+
+    Response findPatient(BigInteger id);
+
+    Response getPatientsList();
+
+
 }
