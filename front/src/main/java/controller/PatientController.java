0f48bda8829a90d9254fc8294a7b00361554db9b
Bouteveille jerome <formation@community-technologie.fr>
2023-04-28 13:06:46 +0200

    Change Param in getPatient()

--- front/src/main/java/controller/PatientController.java
+++ front/src/main/java/controller/PatientController.java
@@ -7,13 +7,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import proxies.UserProxy;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
+import java.math.BigInteger;
 import java.util.List;
 
 @Controller
@@ -37,34 +36,16 @@ public class PatientController {
     @PostMapping(value="/addPatient")
     public void addPatient(HttpServletRequest request){
         userProxy.addPatient(request);
-    } //TODO Garder les servlet car le user interagis avec la vue
-
-    /*@GetMapping(value="/updatePatient/{firstname}{lastname}{birthdate}")
-    public String getUpdatePatient(Model map,
-                                   @RequestParam("firstname") String firstname,
-                                   @RequestParam("lastname") String lastname,
-                                   @RequestParam("birthdate") String birthdate){
-        Response response = userProxy.getPatient(firstname, lastname, birthdate);
-        if(response.getStatus().equals(EnumResponse.OK)) {
-            PatientBean patientBean = modelMapper.map(response.getContent(), PatientBean.class);
-            map.addAttribute("patient", patientBean);
-        } else {
-            map.addAttribute("response", response);
-        }
-        return "updatePatient";
-    }*/
+    }
 
     @PostMapping(value="/updatePatient")
     public void updatePatient(HttpServletRequest request){
         userProxy.updatePatient(request);
     }
 
-    @GetMapping(value="/patient")
-    public String getPatientDetails(Model map,
-                                    @RequestParam("firstname") String firstname,
-                                    @RequestParam("lastname") String lastname,
-                                    @RequestParam("birthdate") String birthdate){
-        ResponseBean response = userProxy.getPatient(firstname, lastname, birthdate);
+    @GetMapping(value="/patient/{id}")
+    public String getPatientDetails(Model map, @RequestParam("id") BigInteger id){
+        ResponseBean response = userProxy.getPatient(id);
         if(response.getStatus().equals(EnumResponse.OK)) {
             PatientBean patientBean = modelMapper.map(response.getContent(), PatientBean.class);
             map.addAttribute("patient", patientBean);
