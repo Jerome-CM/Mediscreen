05e9674a544bd4231638bed93b0aa9e4f3070126
Bouteveille jerome <formation@community-technologie.fr>
2023-04-28 09:33:36 +0200

    Add response

--- front/src/main/java/beans/ResponseBean.java
+++ front/src/main/java/beans/ResponseBean.java
@@ -1,2 +1,20 @@
-package beans;public class ResponseBean {
+package beans;
+
+import entity.EnumResponse;
+import lombok.Data;
+
+@Data
+public class ResponseBean {
+
+    private EnumResponse status;
+
+    private Object content;
+
+    private String message;
+
+    public ResponseBean(EnumResponse status, Object content, String message) {
+        this.status = status;
+        this.content = content;
+        this.message = message;
+    }
 }
