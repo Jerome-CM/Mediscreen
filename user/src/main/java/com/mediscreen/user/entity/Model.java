5f6c4ae15497a1ca76fa9ffb8db48621dc7cd2a6
Bouteveille jerome <formation@community-technologie.fr>
2023-04-28 13:27:58 +0200

    Change type for id

--- user/src/main/java/com/mediscreen/user/entity/Model.java
+++ user/src/main/java/com/mediscreen/user/entity/Model.java
@@ -4,11 +4,13 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.math.BigInteger;
+
 @Data
 public abstract class Model {
 
     @Id
-    private Long id;
+    private BigInteger id;
 
     @NotNull(message = "Firstname is mandatory")
     private String firstname;
